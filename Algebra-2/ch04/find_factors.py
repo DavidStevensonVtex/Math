82ef8acb35c4df7312aa17abfca4e41304008948
David Stevenson <drstevenson1958@yahoo.com>
2025-04-21 12:45:04 -0400

    Algebra 2 - Chapter 4: Radical Expressions and Equations - 4.1 Simplifying Radicals - Check Your Understanding of Section 4.1 - B. Show how you arrived at your answers.

--- Algebra-2/ch04/find_factors.py
+++ Algebra-2/ch04/find_factors.py
@@ -1,5 +1,3 @@
-Python
-
 def find_factors(num):
     factors = []
     for i in range(1, int(num**0.5) + 1):
@@ -10,7 +8,8 @@ def find_factors(num):
     factors.sort()
     return factors
 
-number = 36
+
+number = 48
 factors_of_number = find_factors(number)
 print(f"Factors of {number} are: {factors_of_number}")
 # Expected Output: Factors of 36 are: [1, 2, 3, 4, 6, 9, 12, 18, 36]
